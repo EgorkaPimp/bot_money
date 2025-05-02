28bec1b73ce93a9bfec1f4ba3e7c9679ec02e8e4
Egorka_Pimp <egorkak1408@gmail.com>
2025-05-02 20:47:00 +0300

    [Pimp] change{create_db}, add{2_new_table, waiting_income, waiting_expenses}

--- token_file.py
+++ token_file.py
@@ -1,4 +1,4 @@
 def read_file():
-    with open('Q:\\tg_bot\\money.txt', 'r', encoding='utf-8') as file:
+    with open('/home/egor/token/money.txt', 'r', encoding='utf-8') as file:
         token = file.read()
     return token
