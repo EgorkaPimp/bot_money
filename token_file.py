d1f5a58ff0852e7f6a824ff91eba96a1570bbc8e
EgorkaPimp <egorkak1408@gmail.com>
2025-05-09 19:01:25 +0300

    Pimp [new_pc]

--- token_file.py
+++ token_file.py
@@ -1,4 +1,4 @@
 def read_file():
-    with open('Q:\\my_pet_projact\\money_test.txt', 'r', encoding='utf-8') as file:
+    with open('/home/egor/token/money.txt', 'r', encoding='utf-8') as file:
         token = file.read()
     return token
